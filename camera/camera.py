d0ba05ad33e15815ba15263583cf44c1844643da
Maoxu Li <li@maoxuli.com>
2025-07-06 23:41:10 -0400

    bug fix

--- camera/camera.py
+++ camera/camera.py
@@ -309,6 +309,7 @@ class WebsocketServer(object):
         self._port = port 
         self._connections = set() 
         self._server = None 
+        self._thread = None 
 
     @property 
     def port(self): 
@@ -347,22 +348,29 @@ class WebsocketServer(object):
         if request.path == "/healthz":
             return connection.respond(HTTPStatus.OK, "OK\n")
 
-    async def run_forever(self):
-        if self._server is None: 
-            logger.info(f"Start websocket server at port: {self.port}") 
-            self._server = await websockets.serve(self.handle_client, "", self.port, process_request=self.health_check)
-            await self._server.serve_forever()
+    def run_forever(self):
+        async def _run(): 
+            if self._server is None: 
+                logger.info(f"Start websocket server at port: {self.port}") 
+                self._server = await websockets.serve(self.handle_client, "", self.port, process_request=self.health_check)
+                await self._server.serve_forever()
+        asyncio.run(_run()) 
 
     def start(self): 
-        asyncio.run(self.run_forever())
+        if self._thread is None: 
+            logger.info(f"Start web server at port {self.port}") 
+            self._thread = threading.Thread(target=self.run_forever)
+            self._thread.start()
     
     def stop(self): 
-        if self._server is not None: 
-            logger.warning("Stop websocket server...")
+        if self._thread is not None: 
+            logger.warning("Stop web server...")
             self._server.close()
-            self._server.wait_closed()
+            self._server.wait_closed() 
             self._server = None 
-            logger.warning("Websocket server stopped")
+            self._thread.join()
+            self._thread = None 
+            logger.warning("Web server stopped")
 
 
 # start camera server(s) based on config file 
